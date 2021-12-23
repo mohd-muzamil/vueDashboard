5a5ce2879e0eb15c72df0c0d8f3685c830b73426
mohd-muzamil <mohd.muzamil.08@gmail.com>
2021-12-23 04:34:09 +0000

    one

---
+++ backend/appy.py
@@ -0,0 +1,28 @@
+##################################################################
+#Flask script to generate API that will supply data to the frontend
+#dashboard through HTTP requests
+##################################################################
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS``
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
+if __name__ == '__main__':
+    app.run()
